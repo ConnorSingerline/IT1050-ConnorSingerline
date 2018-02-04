17587c5bf43977b1d82740e91b3374fe57c84fd6
ConnorSingerline <connor.singerline@gmail.com>
2018-02-04 15:19:28 -0500

    Lab01 Upload

---
+++ Lab01/Lab01/Lab01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = "Hello World from ";
+            var y = "Connor Singerline";
+            Console.WriteLine(x + y);
+            Console.ReadLine();
+        }
+    }
+}
