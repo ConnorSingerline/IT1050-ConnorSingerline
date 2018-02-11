53a847c79ba89cfcbcd59f36f36b883fe152cd57
ConnorSingerline <connor.singerline@gmail.com>
2018-02-11 15:59:16 -0500

    Adding Lab02

---
+++ Lab02/Lab02-ConnorSingerline/Lab02-ConnorSingerline/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_ConnorSingerline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("We will add two integers together.\n Type the first integer and click enter:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second integer and click enter:");
+            b = int.Parse(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine($"The total is: {sum}\n\n");
+            Console.WriteLine("Here is a formula (x + y) * (z + 10)\nWe will now assign variables.\nType an integer for x:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type an integer for y:");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type an integer for z:");
+            int z = int.Parse(Console.ReadLine());
+            int sum2 = ((x + y) * (z + 10));
+            Console.WriteLine($"\nThe total is: {sum2}\n\n");
+            Console.WriteLine("Below is an output of messages using escape sequences.\n\nHello\tWorld!\nHello\nWorld\n\"Hello World!\"\nHello\\World!\n\nClick Enter to close the program.");
+            Console.ReadLine();
+        }
+    }
+}
