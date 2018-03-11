8d08ee004c8c18d4d43e9d01a500c5f3761ee026
ConnorSingerline <36058304+ConnorSingerline@users.noreply.github.com>
2018-03-11 16:27:36 -0400

    Add files via upload

---
+++ Test1/Test1_Connor_Singerline/Test1_Connor_Singerline/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_Connor_Singerline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool icyRain = false;
+            bool tornadoWarning = false;
+            if ((icyRain == false) && (tornadoWarning == false))
+            {
+                Console.WriteLine("Let’s go outside!");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+//QUESTION 1
+
+//    using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApplication2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            bool keepLooping = true;
+//            int counter = 1;
+//            while (keepLooping)
+//            {
+//                Console.WriteLine("Loop iteration");
+//                counter++;
+//            }
+//        }
+//    }
+//}
+
+//QUESTION 2
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApplication2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int i = 2;
+//            Console.Write("[");
+//            Console.Write((i));
+//            Console.Write("]");
+//            Console.WriteLine(" ");
+//            while (i < 128)
+//            {
+//                i = i * 2;
+//                Console.Write("[");
+//                Console.Write((i));
+//                Console.Write("]");
+//                Console.WriteLine(" ");
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+
+//QUESTION 3
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApplication2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            for (int num = 49; num > 0;)
+//            {
+//                if (num == 1)
+//                {
+//                    Console.Write(num);
+//                    num--;
+//                    Console.WriteLine(" ");
+//                }
+//                else
+//                {
+//                    Console.Write(num);
+//                    num--;
+//                    Console.Write(", ");
+//                }
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+
+//QUESTION 4
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApplication2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int num = 1;
+//            while (num < 22)
+//            {
+//                Console.Write(num);
+//                Console.Write("   ");
+//                num++;
+//                num++;
+//            }
+//            Console.WriteLine(" ");
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+
+//QUESTION 5
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Test1_Connor_Singerline
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = 8;
+//            int i = 10;
+//            while (i < n)
+//            {
+//                Console.Write("*");
+//                i++;
+//            }
+//            // in a while loop the condition is checked before the inner code is run. On the do while example the code is ran then the condition is checked
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//QUESTION 6
+
+//    using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Test1_Connor_Singerline
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            bool icyRain = false;
+//            bool tornadoWarning = false;
+//            if ((icyRain == false) && (tornadoWarning == false))
+//            {
+//                Console.WriteLine("Let’s go outside!");
+//                Console.ReadLine();
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
